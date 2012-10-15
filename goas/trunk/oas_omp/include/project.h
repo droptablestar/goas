b15caec2973635654b21ea71c6f1cb9cb0103a67
Christian <lalanne@gmail.com>
2012-10-15 02:35:45 +0100

    * ADD: new branch

--- goas/trunk/oas_omp/include/project.h
+++
@@ -1,8 +0,0 @@
-#ifndef _PROJECT_H_
-#define _PROJECT_H_
-
-extern r_list *project(r_list *, const char **);
-extern r_list *par_project(r_list *, unsigned int);
-extern r_list *seq_project(r_list *, unsigned int);
-
-#endif /*!_PROJECT_H_*/
