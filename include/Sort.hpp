37e91635936e6ef3144261409f9c5d80cada6b72
Christian <lalanne@gmail.com>
2013-01-15 00:24:50 +0000

    * ADD: first approach to sorting

---
+++ include/Sort.hpp
@@ -0,0 +1,11 @@
+#ifndef SORT_9Q8EF9HWEF9H9FH9WEF8H
+#define SORT_9Q8EF9HWEF9H9FH9WEF8H
+
+#include <vector>
+#include <string>
+
+class Relation;
+
+void sort(Relation& relation, std::vector<std::string>& keys, bool order);
+
+#endif //SORT_9Q8EF9HWEF9H9FH9WEF8H
